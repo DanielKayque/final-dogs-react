6dcc357dc1a4fcbfc49d9e9e12792ff21f7dff58
Daniel Kayque <daanielkayque@gmail.com>
2025-11-19 13:02:36 -0300

    alterações no title, e footer, configs deploy

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,6 +5,13 @@ const Footer = () => {
   return (
     <div className=" flex items-center justify-center h-44 gap-2 bg-[#FABD01] flex-col">
       <Dogs />
+      <p className="text-gray-500">
+        Vale ressaltar que esse projeto ainda está em desenvolvimento, irá
+        apresentar falhas e pequenos bugs que logo serão corrigidos.
+      </p>
+      <a className="text-gray-800" href="mailto:daanielkayque@gmail.com">
+        daanielkayque@gmail.com
+      </a>
       <p>Dogs. Alguns direitos reservados.</p>
     </div>
   );
